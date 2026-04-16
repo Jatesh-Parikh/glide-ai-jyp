ca590886d15961c2edc7178544b15cb6d81ee0b4
Jatesh Parikh <jateshparikh97@gmail.com>
2026-04-16 21:31:28 +0530

    add GlideIcon component

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { Content } from "@prismicio/client";
+
+defineProps<{ settings?: Content.SettingsDocument }>();
+
+const isOpen = ref(false);
+</script>
+
+<template>
+    <header class="p-4 md:p-6">
+        <nav
+            class="mx-auto flex max-w-6xl flex-col justify-between py-2 md:flex-row md:items-center font-medium text-white"
+            aria-label="Main"
+        >
+            <div class="flex items-center justify-between">
+                
+            </div>
+        </nav>
+    </header>
+</template>
