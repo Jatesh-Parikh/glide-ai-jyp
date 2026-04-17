cdfbafaea6dae26ba4d33e7e4c226ef3a1d8ccf2
Jatesh Parikh <jateshparikh97@gmail.com>
2026-04-17 23:08:42 +0530

    add cta slice

--- app/slices/CallToAction/index.vue
+++ app/slices/CallToAction/index.vue
@@ -14,11 +14,18 @@ defineProps(
 </script>
 
 <template>
-  <section
+  <Bounded
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
+    class="relative py-32 text-center font-medium md:py-40"
   >
-    Placeholder component for call_to_action (variation: {{ slice.variation }})
-    Slices
-  </section>
+    <div class="glow absolute -z-10 aspect-square w-full max-w-sm rounded-full bg-sky-500/50 blur-[160px] filter" />
+    <div class="glass-container rounded-lg bg-gradient-to-b from-gray-800 to-gray-900 p-4 md:rounded-xl">
+      <GlideIcon class="w-24 h-24" />
+    </div>
+    <PrismicText :field="slice.primary.heading" wrapper="h2" class="mt-8 max-w-xl text-balance text-5xl" />
+    <div class="flex gap-8 flex-wrap">
+      <PrismicLink v-for="cta in slice.primary.ctas" :key="cta.text" :field="cta" class="buttonLink mt-6" />
+    </div>
+  </Bounded>
 </template>
